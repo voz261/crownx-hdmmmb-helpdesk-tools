d7bcc2a57f807b9c2860155a870d06e52d79edba
t.na <voz261@gmail.com>
2026-07-05 15:33:41 +0700

    Update helpdesk-tools.user.js

--- helpdesk-tools.user.js
+++ helpdesk-tools.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Helpdesk Tools
 // @namespace    https://github.com/voz261/crownx-hdmmmb-helpdesk-tools
-// @version      1.1.1
+// @version      1.1.2
 // @description  tuanna3
 // @author       tuanna3
 // @match        https://helpdesk.crownx.com.vn/*
